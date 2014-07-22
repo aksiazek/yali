e097e03f1aa7b070bb4aa92dcdaed132d2f6a063
aksiazek <alexander.ksiazek@gmail.com>
2014-07-23 00:47:42 +0200

    Added lisp tests

---
+++ test.lisp
@@ -0,0 +1,19 @@
+(setq factorial (lambda (n) (if (eq n 0) 1 (* (factorial (- n 1)) n))))
+(factorial 5)
+
+(quote a)
+(quote (a b c))
+
+(atom (quote a))
+(atom (quote (a b c)))
+(atom (quote ()))
+(atom (atom (quote a)))
+(atom (quote (atom (quote a))))
+
+(eq (quote a) (quote a))
+(eq (quote a) (quote b))
+(eq (quote ()) (quote ()))
+
+(car (quote (a)))
+(car (quote (a b c)))
+
